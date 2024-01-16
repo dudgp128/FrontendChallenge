87117494d7b87445f2c608a916739f55c6c071b3
dudgp128 <nbv19@naver.com>
2024-01-16 15:09:15 +0900

    [STYLE] ContentIem 레이아웃 구성하기

--- src/order/ContentItem.js
+++ src/order/ContentItem.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import styled from "styled-components";
+import { Title } from "./Title";
 
 const ItemContainer = styled.div`
   width: 100%;
@@ -7,6 +8,45 @@ const ItemContainer = styled.div`
   border-radius: 10px;
   border: 1px solid rgba(0, 0, 0, 0.3);
   background: rgba(247, 90, 47, 0.1);
+  padding: 9px 12px;
+  box-sizing: border-box;
+  display: flex;
+  gap: 8px;
+
+  & > .imgWrapper {
+    height: 100%;
+    aspect-ratio: 1/1;
+    background: #d9d9d9;
+  }
+`;
+
+const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 100%;
+
+  & > div {
+    display: flex;
+    align-items: center;
+  }
+
+  & > .bottom {
+    justify-content: space-between;
+
+    & > .counterBox {
+      display: flex;
+
+      & > span {
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        display: flex;
+        width: 18px;
+        height: 18px;
+      }
+    }
+  }
 `;
 const ContentItem = ({ key, name, event, price }) => {
   const [count, setCount] = useState(0);
@@ -14,19 +54,17 @@ const ContentItem = ({ key, name, event, price }) => {
   return (
     <ItemContainer>
       <div className="imgWrapper" />
-      <div>
-        <div>
-          <div>{name}</div>
-          <div>{event}</div>
+      <InfoContainer>
+        <Title name={name} event={event} />
+        <div className="bottom">
+          <div className="counterBox">
+            <span>-</span>
+            <span>{count}</span>
+            <span>+</span>
+          </div>
+          <div>{price}원</div>
         </div>
-        <div>
-          <div>-</div>
-          <div>{count}</div>
-          <div>+</div>
-        </div>
-        <div>{price}원</div>
-      </div>
-      <div></div>
+      </InfoContainer>
     </ItemContainer>
   );
 };
---
+++ src/order/Title.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+const TitleBox = styled.div`
+  & > div {
+    white-space: pre;
+  }
+  & > .eventBox {
+    display: flex;
+    box-sizing: border-box;
+    border-radius: 10px;
+    background: #f75a2f;
+    padding: 4px;
+    align-items: center;
+
+    & > span {
+      color: #fff;
+      text-align: center;
+
+      font-family: Roboto;
+      font-size: 13px;
+      font-style: normal;
+      font-weight: 400;
+    }
+  }
+`;
+
+export const Title = ({ name, event }) => {
+  return (
+    <TitleBox>
+      <div> {name} </div>
+      {event ? (
+        <div className="eventBox">
+          <span>이벤트</span>
+        </div>
+      ) : null}
+    </TitleBox>
+  );
+};
