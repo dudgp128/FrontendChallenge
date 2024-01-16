5a8a7020f3f8a4e97ee01f6a906c62d9d0bf8b89
dudgp128 <nbv19@naver.com>
2024-01-16 12:13:35 +0900

    [FEAT] 주문 페이지 구성하기

---
+++ src/order/LowerBox.js
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 170px;
+  border-radius: 20px 20px 0px 0px;
+  background: #fff;
+  box-shadow: 0px -4px 10px 0px rgba(0, 0, 0, 0.25);
+  padding: 23px 24.5px 27px 24.5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  box-sizing: border-box;
+
+  & > .textBox {
+    text-align: right;
+    color: #000;
+    font-family: Inter;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  }
+
+  & > .buttonBox {
+    display: flex;
+    justify-content: center;
+    height: 47.919px;
+    background: #c1c1c1;
+
+    & > div {
+      align-self: center;
+
+      color: #fff;
+      text-align: center;
+      font-family: Inter;
+      font-size: 18px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+    }
+  }
+`;
+const LowerBox = () => {
+  return (
+    <Wrapper>
+      <div className="textBox">
+        <div> 총 수량 : {0}개</div>
+        <div> 총 가격 : {0}원 </div>
+      </div>
+      <div className="buttonBox">
+        <div>주문하기</div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default LowerBox;
--- src/order/OrderPage.js
+++ src/order/OrderPage.js
@@ -1,5 +1,38 @@
+import styled from "styled-components";
+import { Logo } from "../common/Logo";
+import LowerBox from "./LowerBox";
+
+const Container = styled.div`
+  width: 350px;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  justify-content: space-between;
+
+  & > .upper {
+    width: 100%;
+    height: 57px;
+    background-color: #000;
+    box-sizing: border-box;
+    padding: 12.5px;
+  }
+
+  & > .content {
+    width: 100%;
+    height: calc(100vh - 57px - 170px); // 전체 화면 - 상단 - 하단
+  }
+`;
 const OrderPage = () => {
-  return <div>OrderPage</div>;
+  return (
+    <Container>
+      <div className="upper">
+        <Logo width={"94.11"} />
+      </div>
+      <div className="content" />
+      <LowerBox />
+    </Container>
+  );
 };
 
 export default OrderPage;
