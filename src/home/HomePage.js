ed52c8158e212c1c41c258b121e959f8f359ecdf
dudgp128 <nbv19@naver.com>
2024-01-16 11:49:46 +0900

    [FEAT] 홈 화면 만들기

--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -1,6 +1,65 @@
+import styled from "styled-components";
+import { Logo } from "../common/Logo";
+import { useNavigate } from "react-router-dom";
+
+const Container = styled.div`
+  width: 350px;
+  height: 100vh;
+  background: #000;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  gap: 41px;
+  justify-content: center;
+
+  & > div {
+    display: flex;
+    justify-content: center;
+
+    & > .gotoOrder {
+      display: flex;
+      width: fit-content;
+      height: 45px;
+      flex-direction: column;
+      justify-content: center;
+      flex-shrink: 0;
+      padding: 21px 11px 22px 11px;
+      border-radius: 20px;
+
+      color: #000;
+      text-align: center;
+      font-family: Inter;
+      font-size: 25px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+      background-color: #fff;
+
+      &:hover {
+        cursor: pointer;
+      }
+    }
+  }
+`;
 const HomePage = () => {
-  return;
-  <div>HomePage</div>;
+  const navigate = useNavigate();
+
+  const onClick = () => {
+    navigate("/order");
+  };
+
+  return (
+    <Container>
+      <div>
+        <Logo />
+      </div>
+      <div>
+        <div className="gotoOrder" onClick={onClick}>
+          주문하러 가기
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
