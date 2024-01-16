f1c6c71376909677f336452391fc82107e77aee4
dudgp128 <nbv19@naver.com>
2024-01-16 13:26:36 +0900

    [FEAT] 서버에서 item 정보 불러오기

--- src/order/ContentList.js
+++ src/order/ContentList.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const FlexList = styled.div`
   display: flex;
@@ -8,6 +10,16 @@ const FlexList = styled.div`
   padding-left: 25px;
 `;
 const ContentList = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get("http://localhost:3001/items");
+        console.log(data);
+      } catch (error) {}
+    };
+    fetchData();
+  }, []);
+
   return <FlexList></FlexList>;
 };
 
